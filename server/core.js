ce65e60ea2ae42a73b6e513f3da922d90a9fc1c5
infoxagc <105364350+infoxagc@users.noreply.github.com>
2022-12-05 00:20:48 +0700

    Update core.js

--- server/core.js
+++ server/core.js
@@ -1,21 +1,10 @@
 const serverless = require("serverless-http");
-const fs = require("fs");
+const { getImages, getSentences } = require("./api");
+const { getFile, validStr, ucwords } = require("./utils");
 const express = require("express");
+
 const app = express();
-
 app.use("/assets", express.static("assets"));
-
-const getFile = async (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(process.cwd() + "/" + path, "utf-8", (err, data) => {
-      if (err) {
-        resolve("err");
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
 
 const Head = async (data) => {
   try {
@@ -34,6 +23,27 @@ app.get("/", async (req, res) => {
   let layout = await getFile("views/layout.html");
   layout = layout.replace("[HEAD]", head).replace("[CONTENTS]", contents);
   res.send(layout);
+});
+
+app.get("/cdn/:query.png", async (req, res) => {
+  let query = await req.params;
+  res.send(query);
+});
+
+app.get("/:query", async (req, res) => {
+  let query = await req.params.query;
+  query = await validStr(query);
+
+  let img = await getImages(query);
+  let sentences = await getSentences(query);
+  res.write(
+    `<h1>${ucwords(query)}</h1>\n<img src="${
+      img[Math.floor(Math.random() * img.length)]["image"]
+    }" width="100%" />\n<p>${
+      sentences[Math.floor(Math.random() * sentences.length)]
+    }</p>`
+  );
+  res.send();
 });
 
 module.exports = app;
